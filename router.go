f0a88d86aec5a7f38fa99bcfecf3cdc0c33313f6
hanshijie <hanshijie@baidu.com>
2025-01-17 18:08:34 +0800

    refactor: move ServeHTTP method from router to Server

--- router.go
+++ router.go
@@ -1,13 +1,8 @@
 package golite
 
 import (
-	"context"
-	"github/hsj/golite/env"
-	"github/hsj/golite/logger"
-	"log"
 	"net/http"
 	"strings"
-	"time"
 )
 
 type Router struct {
@@ -65,79 +60,4 @@ func (r *Router) Route(method, path string) (Controller, map[string]string, bool
 		return trie.Get(path)
 	}
 	return nil, nil, false
-}
-
-func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	ctx := logger.WithContext(req.Context())
-
-	logInst, err := logger.NewLogger(ctx, env.ConfDir())
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-	panicLogInst, err := logger.NewPanicLogger(ctx, env.ConfDir())
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	ctx = WithContext(ctx)
-	gcx := GetContext(ctx)
-	gcx.SetContextOptions(WithRequest(req), WithResponseWriter(w), WithLogger(logInst), WithPanicLogger(panicLogInst))
-
-	logger.AddInfo(ctx, "method", gcx.request.Method)
-	logger.AddInfo(ctx, "url", gcx.request.URL)
-
-	controller, params, ok := r.Route(req.Method, req.URL.Path)
-	if !ok {
-		w.WriteHeader(http.StatusNotFound)
-		return
-	}
-	if params != nil {
-		gcx.SetContextOptions(WithRouterParams(params))
-	}
-
-	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
-	defer cancel()
-
-	doneChan := make(chan struct{}, 1)
-	panicChan := make(chan any, 1)
-
-	go func() {
-		defer func() {
-			if p := recover(); p != nil {
-				panicLogInst.Report(ctx, p)
-				panicChan <- p
-			}
-		}()
-		ctx = WithTracker(ctx)
-		tracker := GetTracker(ctx)
-		gcx = GetContext(ctx)
-		logInst := gcx.Logger()
-
-		err := controller.Init(ctx)
-		if err != nil {
-			return
-		}
-		err = controller.Serve(ctx)
-		if err != nil {
-			return
-		}
-		err = controller.Finalize(ctx)
-		if err != nil {
-			return
-		}
-		tracker.LogTracker(ctx)
-		logInst.Info(ctx, "ok")
-
-		doneChan <- struct{}{}
-	}()
-
-	select {
-	case p := <-panicChan:
-		log.Printf("%v", p)
-	case <-ctx.Done():
-		log.Print("timeout")
-	case <-doneChan:
-	}
 }
--- server.go
+++ server.go
@@ -1,8 +1,12 @@
 package golite
 
 import (
+	"context"
 	"github/hsj/golite/env"
+	"github/hsj/golite/logger"
+	"log"
 	"net/http"
+	"time"
 )
 
 type Server struct {
@@ -26,7 +30,6 @@ func New(conf string) *Server {
 func (s *Server) Start() error {
 	server := http.Server{
 		Addr:         s.addr,
-		Handler:      &s.router,
 		ReadTimeout:  env.ReadTimeout(),
 		WriteTimeout: env.WriteTimeout(),
 		IdleTimeout:  env.IdleTimeout(),
@@ -48,4 +51,79 @@ func (s *Server) OnPut(path string, controller Controller) {
 
 func (s *Server) OnDelete(path string, controller Controller) {
 	s.router.OnDelete(path, controller)
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	ctx := logger.WithContext(req.Context())
+
+	logInst, err := logger.NewLogger(ctx, env.ConfDir())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	panicLogInst, err := logger.NewPanicLogger(ctx, env.ConfDir())
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	ctx = WithContext(ctx)
+	gcx := GetContext(ctx)
+	gcx.SetContextOptions(WithRequest(req), WithResponseWriter(w), WithLogger(logInst), WithPanicLogger(panicLogInst))
+
+	logger.AddInfo(ctx, "method", gcx.request.Method)
+	logger.AddInfo(ctx, "url", gcx.request.URL)
+
+	controller, params, ok := s.router.Route(req.Method, req.URL.Path)
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+	if params != nil {
+		gcx.SetContextOptions(WithRouterParams(params))
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	doneChan := make(chan struct{}, 1)
+	panicChan := make(chan any, 1)
+
+	go func() {
+		defer func() {
+			if p := recover(); p != nil {
+				panicLogInst.Report(ctx, p)
+				panicChan <- p
+			}
+		}()
+		ctx = WithTracker(ctx)
+		tracker := GetTracker(ctx)
+		gcx = GetContext(ctx)
+		logInst := gcx.Logger()
+
+		err := controller.Init(ctx)
+		if err != nil {
+			return
+		}
+		err = controller.Serve(ctx)
+		if err != nil {
+			return
+		}
+		err = controller.Finalize(ctx)
+		if err != nil {
+			return
+		}
+		tracker.LogTracker(ctx)
+		logInst.Info(ctx, "ok")
+
+		doneChan <- struct{}{}
+	}()
+
+	select {
+	case p := <-panicChan:
+		log.Printf("%v", p)
+	case <-ctx.Done():
+		log.Print("timeout")
+	case <-doneChan:
+	}
 }
